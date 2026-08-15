621609d27f54ed346fca5263fd0273e40d43ee49
Irzan Ahmad <irzanahmad44@gmail.com>
2026-08-15 18:52:48 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Demo from './Demo'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      
+      <Navbar/>
+
+      App
+
+      <Demo/>
+
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default App
